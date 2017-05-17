5f1d62a522c52a866a0ca8de7a8cba69ccec39fe
Yahya Sahaja <yahya.sahaja2@gmail.com>
2017-05-17 10:36:28 +0700

    mLab

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
+const { ObjectID } = require('mongodb');
 const { url } = require('./config/database');
 const { User } = require('./app/model/User');
 
@@ -14,9 +15,22 @@ app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
 
+// var usr = new User();
+// usr.email = 'yahya@mail.com';
+// usr.save().then(doc => console.log(usr), err => console.log(err));
+
 //ROUTER
 app.get('/todos', (req, res) => {
     User.count().then(n => res.send(`we have ${n} data`), err => res.send(`Unable to fetch data: ${err}`));
+});
+
+app.get('/todos/:id', (req, res) => {
+    let { id } = req.params;
+
+    User.findById(id).then(user => {
+        if (!user) return res.status(404).send(`User with id ${id} is not found`);
+        res.status(200).send(`User email is: ${user.email}`);
+    }, err => res.status(400).send('Bad request'));
 });
 
 app.post('/todos', (req, res) => {
--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/TodoApp'
+    url: 'mongodb://todoapp:todo123@ds141401.mlab.com:41401/todoapp'
 };
